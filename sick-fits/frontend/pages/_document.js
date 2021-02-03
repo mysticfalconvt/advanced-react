0456b3d3e17fcf002f2548f79791eef5f3683573
MysticfalconVT <rboskind@gmail.com>
2021-02-03 15:36:22 -0500

    Done 6

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -2,12 +2,14 @@ import Document, { Html, Head, NextScript, Main } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
-    <Html lang="en-US">
-      {/* {<Head> </Head>} */}
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>;
+    return (
+      <Html lang="en-US">
+        {/* <Head></Head> */}
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
