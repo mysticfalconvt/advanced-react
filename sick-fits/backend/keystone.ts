b431cdd1a199da7051c339f2c8e583530b535ed0
MysticfalconVT <rboskind@gmail.com>
2021-02-06 13:56:01 -0500

    done 16

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -2,6 +2,7 @@ import { config, createSchema } from '@keystone-next/keystone/schema';
 import 'dotenv/config';
 import {User} from './schemas/User'
 import {Product} from './schemas/Product'
+import {ProductImage} from './schemas/ProductImage'
 import {createAuth} from '@keystone-next/auth'
 import {withItemData, statelessSessions} from '@keystone-next/keystone/session'
 
@@ -40,6 +41,7 @@ export default withAuth(config({
     // Schema items go in here
     User,
     Product,
+    ProductImage,
   }),
   ui: {
     // show UI for people who passs the test
