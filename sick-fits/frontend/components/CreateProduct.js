ff1a08aeb1a0f51118cbb491ada0dfda501b81fe
MysticfalconVT <rboskind@gmail.com>
2021-02-06 19:30:50 -0500

    done 22

---
+++ sick-fits/frontend/components/CreateProduct.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import useForm from "../lib/useForm";
+
+export default function CreateProduct() {
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
+    name: "wes",
+    price: 12345,
+    description: "Nice Shoes bud",
+  });
+  return (
+    <form>
+      <label htmlFor="name">
+        Name
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          value={inputs.name}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="price">
+        Name
+        <input
+          type="number"
+          id="price"
+          name="price"
+          placeholder="Price"
+          value={inputs.price}
+          onChange={handleChange}
+        />
+      </label>
+      <button type="button" onClick={clearForm}>
+        Clear Form
+      </button>
+      <button type="button" onClick={resetForm}>
+        Reset Form
+      </button>
+    </form>
+  );
+}
