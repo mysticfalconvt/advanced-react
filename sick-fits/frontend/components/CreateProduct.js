3a98d587767a80aea8ad26ac32ccf0a78b127967
MysticfalconVT <rboskind@gmail.com>
2021-02-07 07:43:28 -0500

    done 24

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,6 +1,33 @@
+import { gql, useMutation } from "@apollo/client";
 import { useState } from "react";
 import useForm from "../lib/useForm";
 import Form from "./styles/Form";
+import DisplayError from "./ErrorMessage";
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # Which variables get passed in and what type are they
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
@@ -9,14 +36,25 @@ export default function CreateProduct() {
     price: 12345,
     description: "Nice Shoes bud",
   });
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    },
+  );
+
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
-        console.log(inputs);
+
+        // submit the input fields to the backend
+        await createProduct();
+        clearForm();
       }}
     >
-      <fieldset>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
           <input
@@ -60,13 +98,7 @@ export default function CreateProduct() {
             onChange={handleChange}
           />
         </label>
-        <button type="submit">+ Add Prouct</button>
-        <button type="button" onClick={clearForm}>
-          Clear Form
-        </button>
-        <button type="button" onClick={resetForm}>
-          Reset Form
-        </button>
+        <button type="submit">+ Add Product</button>
       </fieldset>
     </Form>
   );
