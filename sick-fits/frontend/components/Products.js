0bdd63a873adbf6791a92be702f5e45aac22d728
MysticfalconVT <rboskind@gmail.com>
2021-02-13 09:34:53 -0500

    done 34

--- sick-fits/frontend/components/Products.js
+++ sick-fits/frontend/components/Products.js
@@ -1,18 +1,19 @@
 import { useQuery } from "@apollo/client";
-import qkl from "graphql-tag";
+import gql from "graphql-tag";
 import styled from "styled-components";
+import { perPage } from "../config";
 import Product from "./Product";
 
-export const ALL_PRODUCTS_QUERY = qkl`
-query ALL_PRODUCTS_QUERY{
-    allProducts{
+export const ALL_PRODUCTS_QUERY = gql`
+  query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
+    allProducts(first: $first, skip: $skip) {
       id
       name
       price
       description
-      photo{
+      photo {
         id
-        image{
+        image {
           publicUrlTransformed
         }
       }
@@ -26,8 +27,13 @@ const ProductsListStyles = styled.div`
   grid-gap: 60px;
 `;
 
-export default function Products() {
-  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+export default function Products({ page }) {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
+    variables: {
+      skip: page * perPage - perPage,
+      first: perPage,
+    },
+  });
   // console.log(data, error, loading);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
